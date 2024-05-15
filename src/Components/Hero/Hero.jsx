48881ebb56a214570052128b3b93640672678f58
Sandun Kanangama <sandunkanangama@gmail.com>
2024-05-15 14:30:46 +0900

    Adding files

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id='home' className='hero'>
+      <img src={profile_img} alt="" className='profile-img' />
+      <h1><span>I'm Alex Bennett,</span> frontend developer based in USA.</h1>
+      <p>I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
+      <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
