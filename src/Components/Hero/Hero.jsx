be4470c844b430381970e195d5267deae4e223f2
Sandun Kanangama <sandunkanangama@gmail.com>
2024-05-21 12:26:10 +0900

    Added resume link

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/sandun_hero_img.jpg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import resume from '../../assets/Sandun_Kanangama_Resume.pdf'
 
 const Hero = () => {
   return (
@@ -15,7 +16,12 @@ const Hero = () => {
       <p>I've come from Melbourne, Australia. I am currently relocating to Japan, and making a transition from Business Analysis into Data and Tech.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <div className="hero-resume">My resume</div>
+        <a href={resume} target="_blank">
+          <div className="hero-resume">
+            My Resume
+          </div>
+        </a>
+
       </div>
     </div>
   )
